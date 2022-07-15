d2edf2ae9c14870b5bdb0ed943b435baac21fd3a
Tyler M <tmorse2222@gmail.com>
2022-07-15 16:11:45 -0700

    Created function for first time block

--- assets/script.js
+++ assets/script.js
@@ -18,13 +18,23 @@ function time(){
 function currentEvent(){
     if (hour = 9){
         nine.addClass(`present`);
-
+        ten.addClass(`future`);
+        eleven.addClass(`future`);
+        twelve.addClass(`future`);
+        one.addClass(`future`);
+        two.addClass(`future`);
+        three.addClass(`future`);
+        four.addClass(`future`);
+        five.addClass(`future`);
+    } else if (){
+        
     }
 }
 
 dateDisplay.ready(function(){
     setInterval(`time()`, 500)});
     
-timeBlock.ready(function(){
-    }
+timeBlock.ready(currentEvent(){
+
+}
 )
