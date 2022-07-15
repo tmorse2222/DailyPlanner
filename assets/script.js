832f9813d33fd4059c8a22c6a08fa58442abe262
Tyler M <tmorse2222@gmail.com>
2022-07-15 16:05:28 -0700

    Created variables for each ID

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,15 @@ var currentDay = new Date;
 var dateDisplay = $(`#currentDay`);
 var hour = currentDay.getHours();
 var timeBlock = $(`.row`);
+var 9am = $(`#9am`);
+var 10am = $(`#10am`);
+var 11am = $(`#11am`);
+var 12pm = $(`#12pm`);
+var 1pm = $(`#1pm`);
+var 2pm = $(`#2pm`);
+var 3pm = $(`#3pm`);
+var 4pm = $(`#4pm`);
+var 5pm = $(`#5pm`);
 
 function time(){
     dateDisplay.text(Date);
