c427cbcc240d763d8e370dae9a1cba3605f1b621
Tyler M <tmorse2222@gmail.com>
2022-07-15 16:07:44 -0700

    Renamed variables

--- assets/script.js
+++ assets/script.js
@@ -2,22 +2,23 @@ var currentDay = new Date;
 var dateDisplay = $(`#currentDay`);
 var hour = currentDay.getHours();
 var timeBlock = $(`.row`);
-var 9am = $(`#9am`);
-var 10am = $(`#10am`);
-var 11am = $(`#11am`);
-var 12pm = $(`#12pm`);
-var 1pm = $(`#1pm`);
-var 2pm = $(`#2pm`);
-var 3pm = $(`#3pm`);
-var 4pm = $(`#4pm`);
-var 5pm = $(`#5pm`);
+var nine = $(`#9am`);
+var ten = $(`#10am`);
+var eleven = $(`#11am`);
+var twelve = $(`#12pm`);
+var one = $(`#1pm`);
+var two = $(`#2pm`);
+var three = $(`#3pm`);
+var four = $(`#4pm`);
+var five = $(`#5pm`);
 
 function time(){
     dateDisplay.text(Date);
 }
 function currentEvent(){
     if (hour = 9){
-        $(`#9am`).addClass(`present`);
+        nine.addClass(`present`);
+
     }
 }
 
