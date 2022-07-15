245d61704b0309ade57929bd6524eaba461ff995
Tyler M <tmorse2222@gmail.com>
2022-07-15 13:40:55 -0700

    Added variable to attach value of current hour to later functions

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,17 @@
-var currentDay = Date;
+var currentDay = new Date;
 var dateDisplay = $(`#currentDay`);
+var hour = currentDay.getHours();
+var timeBlock = $(`.row`);
 
 function time(){
     dateDisplay.text(currentDay);
 }
+function currentEvent(){
+
+}
 
 dateDisplay.ready(function(){
     setInterval(`time()`, 500)});
+timeBlock.ready(function(){
+    
+})    
